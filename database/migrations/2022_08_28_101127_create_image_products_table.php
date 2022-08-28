c5182a488affd408a0d965c9c26d6cd3fc743884
charleschrist21 <charleschrist21@gmail.com>
2022-08-28 17:27:03 +0700

    new update

---
+++ database/migrations/2022_08_28_101127_create_image_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImageProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('image_products', function (Blueprint $table) {
+            $table->id();
+            $table->text('image');
+            $table->foreignId('product_id')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image_products');
+    }
+}
