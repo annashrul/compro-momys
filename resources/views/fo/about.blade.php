fb7d3302b80e01f38447106bbe56e893192137dc
netindo <anashrulyusuf@gmail.com>
2022-12-13 19:17:34 +0700

    add about us

--- resources/views/fo/about.blade.php
+++ resources/views/fo/about.blade.php
@@ -1,25 +1,37 @@
 @extends('fo/index')
 @section('content')
-    <div  data-aos="fade-in" data-aos-duration="4000" style="margin-top: 150px;">
+    <div data-aos="fade-in" data-aos-duration="4000" style="margin-top: 150px;">
         <div class="container">
             <div class="section-title text-center" style="display: flex;flex-direction: column;justify-content: center;">
-                <h2>ABOUT US</h2>
-                <center><div style="margin-top: 15px;border-radius:4px;height: 3px;width: 100px;background-color: white;"></div></center>
+                <h2>Momis Journey</h2>
+                <center>
+                    <div style="margin-top: 15px;border-radius:4px;height: 3px;width: 100px;background-color: white;"></div>
+                </center>
             </div>
             <br>
             <div class="row" style="position: relative">
                 <div class="col-md-4">
-                    <img style="border-radius: 10px" src="https://cdn.shopify.com/s/files/1/0100/4575/1377/files/50-50-Homepage_1400x.jpg?v=1655342627"/>
+                    <img style="border-radius: 10px"
+                        src="https://cdn.shopify.com/s/files/1/0100/4575/1377/files/50-50-Homepage_1400x.jpg?v=1655342627" />
                 </div>
                 <div class="col-md-8" style="">
-                    <h3>What is Lorem Ipsum?</h3>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    Sebelum Tahun 2010 ada Bakery Langganan yang sangat Membantu Memecahkan Masalah Ketika kita Ingin
+                    Berbagi ke Tetangga, Saudara atau Relasi2. Kualitas Premium dengan Harga Dapat diterima Pasar Menengah.
+                    Ukuran yang Mini kami Menyebutnya (Tidak Besar & Tidak Kecil(Unyil)).
+                    Momis Bakery berdiri dengan Tujuan Bisa Berbagi dengan Sesama Tidak Perlu beli ke Brand Lain. Momis
+                    Bakery berdiri Tanpa Ilmu Bisnis. Dengan keilmuan yang belum mempuni di Bidang Kuliner / FnB Founder
+                    belum bisa maksimal Membawa Traffic ke Strore. Founder memutuskan untuk Jemput Bola ( doot to door )
+                    dari Instansi ke Instansi melakukan Penawaran Snack Box untuk Kegiatan di Instansi Tersebut .
+                    Penolakan demi penolakan sudah hal yang biasa bagi Founder. Pantang Menyerah karena ada Tanggungan
+                    seperti Pegawai yang wajib di bayar Setiap Bulan nya.
+                    <p>Atas izin Allah Momis Bakery mulai ada Pesanan Snack Box Mulai dari belasan Box sampai Ribuan Box
+                        untuk Acara Wisuda. Karena Mencari Pelanggan itu Tidak Mudah, Kami benar – benar sangat Menjaga
+                        Kualitas Produk & Pelayanan terhadap Konsumen. Pada saat tim belum banyak, Pelayanan dilakukan
+                        lanagsung oleh Founder. Berbeda dengan Para Kompetitor yang sudah didelegasikan ke Tim</p>
 
                 </div>
             </div>
 
         </div>
     </div>
-
 @endsection
